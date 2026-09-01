aaa796c3100a0dabe1b1ffaae3489d50d4b70ba8
Boris Mahne-Becker <50378183+Boris26@users.noreply.github.com>
2026-09-01 14:10:08 +0200

    Connect warnings to mobile production view

--- src/containers/Mobile/MobileStatusView/MobileProductionView.connect.ts
+++ src/containers/Mobile/MobileStatusView/MobileProductionView.connect.ts
@@ -2,15 +2,18 @@ import { connect } from 'react-redux';
 import { ProductionActions } from '../../../actions/actions';
 import {MobileProductionView} from './MobileProductionView';
 import {ConfirmStates} from '../../../enums/eConfirmStates';
+import type {RootState} from '../../../reducers/rootReducer';
 
-const mapStateToProps = (state: any) => ({
+const mapStateToProps = (state: RootState) => ({
     temperature: state.productionReducer.temperature,
     brewingStatus: state.productionReducer.brewingStatus,
     isConfirmPending: state.productionReducer.isConfirmPending,
     confirmError: state.productionReducer.confirmError,
-    isBrewingStatusStale: state.productionReducer.isBrewingStatusStale
-    ,realtimeState: state.productionReducer.realtimeState
-    ,socketConnected: state.productionReducer.socketConnection.connected
+    isBrewingStatusStale: state.productionReducer.isBrewingStatusStale,
+    realtimeState: state.productionReducer.realtimeState,
+    socketConnected: state.productionReducer.socketConnection.connected,
+    warnings: state.warningReducer.warnings,
+    warningsReceived: state.warningReducer.warningsReceived,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
