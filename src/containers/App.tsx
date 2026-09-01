dfb43547a401548ec720cb11ac92e07286e3e731
Boris Mahne-Becker <50378183+Boris26@users.noreply.github.com>
2026-09-01 11:22:11 +0200

    Show heater safety alarm globally

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,9 +1,12 @@
 import React, { Suspense, useEffect } from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Header from './MainView/Header/Header.connect';
 import './App.css';
 import Index from "./index.connect";
 import {ProductionActions} from '../actions/actions';
+import type {RootState} from '../reducers/rootReducer';
+import {isHeaterStuckOnAlarmActive} from '../utils/brewingStatus/alarmDisplay';
+import GlobalHeaterSafetyDialog from '../components/GlobalHeaterSafetyDialog/GlobalHeaterSafetyDialog';
 
 const MobileProductionView = React.lazy(() => import('./Mobile/MobileStatusView/MobileProductionView.connect'));
 
@@ -11,6 +14,13 @@ const App: React.FC = () => {
     const isMobile = window.innerWidth < 768;
     const isDashboardRoute = window.location.pathname === '/dashboard';
     const dispatch = useDispatch();
+    const production = useSelector((state: RootState) => state.productionReducer);
+    const socketConnected = production.socketConnection.connected;
+    const realtime = production.realtimeState;
+    const alarms = socketConnected && realtime.alarmsReceived ? realtime.alarms : undefined;
+    const heaterSafetyAlarmActive = isHeaterStuckOnAlarmActive(alarms);
+    const currentTemperature = socketConnected ? realtime.temperatureSensor?.current : undefined;
+    const heatingRunning = socketConnected ? realtime.heatingRunning : undefined;
 
     useEffect(() => {
         // Desktop owns the same central connection through Index. The mobile
@@ -20,9 +30,18 @@ const App: React.FC = () => {
         return () => { dispatch(ProductionActions.webSocketDisconnect()); };
     }, [dispatch, isDashboardRoute, isMobile]);
 
+    const globalSafetyDialog = (
+        <GlobalHeaterSafetyDialog
+            open={heaterSafetyAlarmActive}
+            temperature={currentTemperature}
+            heatingRunning={heatingRunning}
+        />
+    );
+
     if (isMobile && !isDashboardRoute) {
         return (
             <div className="AppContainer">
+                {globalSafetyDialog}
                 <Suspense fallback={<div className="view-loading" role="status">Lade Ansicht…</div>}>
                     <MobileProductionView/>
                 </Suspense>
@@ -32,6 +51,7 @@ const App: React.FC = () => {
 
     return (
         <div className="AppContainer">
+            {globalSafetyDialog}
             <div className="AppHeader">
                 <Header></Header>
             </div>
