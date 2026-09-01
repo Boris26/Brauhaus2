1142d0aeec87187a055f0791e834b6e93436cd6c
Boris Mahne-Becker <50378183+Boris26@users.noreply.github.com>
2026-09-01 14:04:45 +0200

    Register global warning reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -6,11 +6,13 @@ import { hopsReducer, HopsReducerState, initialHopsState } from './hopsReducer';
 import {maltsReducer, MaltsReducerState, initialMaltsState} from './maltsReducer';
 import {yeastReducer, YeastReducerState, initialYeastState} from './yeastReducer';
 import {additionalIngredientsReducer, AdditionalIngredientsReducerState, initialAdditionalIngredientsState} from './additionalIngredientsReducer';
+import {initialWarningState, warningReducer, WarningReducerState} from './warningReducer';
 
 export const rootReducer = combineReducers({
     applicationReducer: applicationReducer,
     beerDataReducer: beerDataReducer,
     productionReducer: productionReducer,
+    warningReducer: warningReducer,
     hopsReducer: hopsReducer,
     maltsReducer: maltsReducer,
     yeastReducer: yeastReducer,
@@ -22,10 +24,11 @@ export interface RootState {
     applicationReducer: ApplicationReducerState;
     beerDataReducer: BeerDataReducerState;
     productionReducer: ProductionReducerState;
+    warningReducer: WarningReducerState;
     hopsReducer: HopsReducerState;
     maltsReducer: MaltsReducerState;
     yeastReducer: YeastReducerState;
     additionalIngredientsReducer: AdditionalIngredientsReducerState;
 }
-export type { ApplicationReducerState, BeerDataReducerState, ProductionReducerState, HopsReducerState, MaltsReducerState, YeastReducerState, AdditionalIngredientsReducerState };
-export { initialApplicationState, initialBeerState, initialProductionState ,initialHopsState, initialMaltsState, initialYeastState, initialAdditionalIngredientsState};
+export type { ApplicationReducerState, BeerDataReducerState, ProductionReducerState, WarningReducerState, HopsReducerState, MaltsReducerState, YeastReducerState, AdditionalIngredientsReducerState };
+export { initialApplicationState, initialBeerState, initialProductionState, initialWarningState, initialHopsState, initialMaltsState, initialYeastState, initialAdditionalIngredientsState};
