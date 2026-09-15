922d81f8ed12bca0d5081fc0872928e0ae1dfb71
Boris Mahne-Becker <50378183+Boris26@users.noreply.github.com>
2026-09-15 20:33:19 +0200

    style: unify form controls across application

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './colors.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'simplebar/dist/simplebar.min.css';
 import './index.css';
+import './form-controls.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
